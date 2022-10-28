2de0576151885d75932ab6d3e9212a8130542ded
mlbtheroku <115041575+mlbtheroku@users.noreply.github.com>
2022-10-28 08:58:52 +0630

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("API_ID", ))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 
-INDEX_ID = int(getenv("INDEX_ID"))
-UPLOADS_ID = int(getenv("UPLOADS_ID"))
+INDEX_ID = int(getenv("INDEX_ID", ""))
+UPLOADS_ID = int(getenv("UPLOADS_ID", ""))
 
-STATUS_ID = int(getenv("STATUS_ID"))
-SCHEDULE_ID = int(getenv("SCHEDULE_ID"))
+STATUS_ID = int(getenv("STATUS_ID", ""))
+SCHEDULE_ID = int(getenv("SCHEDULE_ID", ""))
 
-CHANNEL_TITLE = getenv("CHANNEL_TITLE")
-INDEX_USERNAME = getenv("INDEX_USERNAME")
-UPLOADS_USERNAME = getenv("UPLOADS_USERNAME")
+CHANNEL_TITLE = getenv("CHANNEL_TITLE", "")
+INDEX_USERNAME = getenv("INDEX_USERNAME", "")
+UPLOADS_USERNAME = getenv("UPLOADS_USERNAME", "")
