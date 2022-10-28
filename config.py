753c0ac1bfc7d25a707823d3f14b1e9513fcdc74
mlbtheroku <115041575+mlbtheroku@users.noreply.github.com>
2022-10-28 16:52:38 +0630

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", 17894641))
 API_HASH = getenv("API_HASH", "4e5b39e5c7c6066e5144dfc50cf466cf")
-BOT_TOKEN = getenv("BOT_TOKEN", "5719320457:AAFBdOvRmPXEBVYdMnLvb4O22z9lOCXUPlU")
+BOT_TOKEN = getenv("BOT_TOKEN", "5456141576:AAG2IB9_m2D4RlfVEA-zm896nDFvnz5ITLQ")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://AutoAnime:AutoAnime@autoanime.f8ahzhs.mongodb.net/?retryWrites=true&w=majority")
 
 INDEX_ID = int(getenv("INDEX_ID", "-1001655574268"))
 UPLOADS_ID = int(getenv("UPLOADS_ID", "-1001730395459"))
 
-STATUS_ID = int(getenv("STATUS_ID", 8))
-SCHEDULE_ID = int(getenv("SCHEDULE_ID", 10))
+STATUS_ID = int(getenv("STATUS_ID", 13))
+SCHEDULE_ID = int(getenv("SCHEDULE_ID", 14))
 
 CHANNEL_TITLE = getenv("CHANNEL_TITLE", "AnimeAuto")
 INDEX_USERNAME = getenv("INDEX_USERNAME", "AutoAnimeIndexChannel")
